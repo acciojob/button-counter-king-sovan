3c3351e84ce6135e7b2214dac9677ac2052f808f
Sovan Bakshi <lionalsovan017@gmail.com>
2024-10-09 17:02:28 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react/cjs/react.production.min";
 
 const App = () => {
+  const[count, setCount] = useState(0)
   return (
     <div>
+      <p>Button cliked {count} times</p>
+      <button onClick = {() => 
+        setCount(count + 1)
+      }>Click me</button>
         {/* Do not remove the main div */}
     </div>
   )
